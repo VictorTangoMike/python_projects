56ef97809586e97e5cbe161b6e02c84d2c99a098
Victor <victorhgabriel3@gmail.com>
2023-12-27 22:58:36 -0300

    code upgrade - adding some functions

--- desafios_dio/banco.py
+++ desafios_dio/banco.py
@@ -3,10 +3,45 @@ from datetime import datetime
 
 opcao = ''
 saldo = 0
-limite_saque = 500
 extrato = ""
 numero_saque = 0
-NUMERO_SAQUE_LIMITE = 3
+LIMITE_SAQUE = 3
+MAX_VALUE_SAQUE = 500
+
+def saque(extrato, saldo, numero_saque):
+    global LIMITE_SAQUE, MAX_VALUE_SAQUE
+    
+    if numero_saque < LIMITE_SAQUE:
+        
+        valor = float(input("Valor: R$"))
+
+        if saldo >= valor:
+            saldo -= valor
+            numero_saque += 1
+            print(f"O valor de R${valor} foi sacado com sucesso.")
+
+            msg = f"{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} - Saque de R${valor}\n"
+
+            extrato += msg            
+        else: 
+            print("Saldo insuficiente")
+    else:
+        print(f"Limite de saque excedido")
+
+    return extrato, saldo, numero_saque
+
+def deposito(extrato, saldo):
+    valor = float(input("Valor: R$"))
+
+    saldo += valor
+
+    print(f"O valor de R${valor} foi depositado em sua conta")
+
+    msg = f"{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} - Deposito de R${valor}\n"
+
+    extrato += msg
+
+    return extrato, saldo
 
 while opcao != "Q":
     opcao = input("""
@@ -23,45 +58,18 @@ while opcao != "Q":
     while True:
         if opcao == "D":
 
-            valor = float(input("Valor: R$"))
-
-            saldo += valor
-
-            print(f"O valor de R${valor} foi depositado em sua conta")
-
-            extrato += f"{datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                          } - Deposito de R${valor}\n"
+            extrato, saldo = deposito(extrato, saldo)
 
             break
 
         elif opcao == "S":
 
-            if numero_saque < NUMERO_SAQUE_LIMITE:
-
-                valor = float(input("Valor: R$"))
-
-                if saldo >= valor:
-                    saldo -= valor
-                    numero_saque += 1
-
-                    print(f"Saldo no valor de R${
-                          valor} foi sacado com sucesso.")
-
-                    extrato += f"{datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                                  } - Saque de R${valor}\n"
-
-                    break
-                else:
-                    print("Saldo insuficiente")
-                    break
-            else:
-                print("Limite de saque excedido")
+            extrato, saldo, numero_saque = saque(extrato, saldo, numero_saque)
 
             break
 
         elif opcao == "E":
-            print(f"Extrato: \n{extrato} \n \n Extrato gerado em: {
-                  datetime.now().strftime("%d/%m/%Y %H:%M:%S")}")
+            print(f"Extrato: \n{extrato} \n \n Extrato gerado em: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}")
 
             break
         elif opcao == "Q":
